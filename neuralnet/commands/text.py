dfe2ff1dc5cac2d553ada62589cc178e240e9c0a
Oscar Eriksson <scorch@stink>
2016-01-03 11:12:56 +0800

    GRU network

--- neuralnet/commands/text.py
+++ neuralnet/commands/text.py
@@ -2,13 +2,15 @@ __author__ = 'Oscar Eriksson <oscar.eriks@gmail.com>'
 
 import logging
 import os
+import numpy
 from .base import ICommand
 from zope.interface import implementer
+from neuralnet.nn.utils import Utils
 
 logger = logging.getLogger(__name__)
 
-
 _VOCABULARY_SIZE = int(os.environ.get('VOCABULARY_SIZE', '8000'))
+_TRAINING_FILE = 'data/RC_2010-02-10k.json'
 
 
 @implementer(ICommand)
@@ -17,6 +19,12 @@ class TextCommand:
     usage = "text <foo>"
     short = "Run some RNN text generation."
     docs = """See the nn.yaml configuration for details."""
+
+    def __init__(self):
+        self.vocabulary_size = _VOCABULARY_SIZE
+        self.unknown_token = "UNKNOWN_TOKEN"
+        self.sentence_start_token = "SENTENCE_START"
+        self.sentence_end_token = "SENTENCE_END"
 
     def setenv(self, env):
         self.env = env
@@ -30,86 +38,138 @@ class TextCommand:
     def execute(self, stage):
         ok = True
 
-        vocabulary_size = _VOCABULARY_SIZE
-        unknown_token = "UNKNOWN_TOKEN"
-        sentence_start_token = "SENTENCE_START"
-        sentence_end_token = "SENTENCE_END"
+        (word_to_index, index_to_word, tokenized_sentences) = Utils.load_csv_data(
+            _TRAINING_FILE, self.sentence_start_token, self.sentence_end_token,
+            self.unknown_token, self.vocabulary_size)
 
-        # Read the data and append SENTENCE_START and SENTENCE_END tokens
-        logger.debug("Reading CSV file...")
-        import simplejson as json
-        import nltk
-        import itertools
-
-        sentences = []
-        with open('data/RC_2010-02-small.json', 'rb') as file:
-            for line in file:
-                post = json.loads(line)
-                if "body" not in post:
-                    continue
-                body = post["body"]
-                sentence = nltk.sent_tokenize(body.lower())
-                sentences.append("%s %s %s" % (sentence_start_token, sentence, sentence_end_token))
-        logger.info("Parsed %d sentences." % (len(sentences)))
-
-        tokenized_sentences = [nltk.word_tokenize(sent) for sent in sentences]
-
-        # Count the word frequencies
-        word_freq = nltk.FreqDist(itertools.chain(*tokenized_sentences))
-        logger.info("Found %d unique words tokens." % len(word_freq.items()))
-
-        # Get the most common words and build index_to_word and word_to_index vectors
-        vocab = word_freq.most_common(vocabulary_size-1)
-        index_to_word = [x[0] for x in vocab]
-        index_to_word.append(unknown_token)
-        word_to_index = dict([(w, i) for i, w in enumerate(index_to_word)])
-
-        logger.debug("Using vocabulary size %d." % vocabulary_size)
-        logger.debug("The least frequent word in our vocabulary is '%s' and appeared %d times." % (vocab[-1][0], vocab[-1][1]))
-
-        # Replace all words not in our vocabulary with the unknown token
-        for i, sent in enumerate(tokenized_sentences):
-            tokenized_sentences[i] = [w if w in word_to_index else unknown_token for w in sent]
-
-        logger.debug("Example sentence: '%s'" % sentences[0])
-        logger.debug("Example sentence after Pre-processing: '%s'" % tokenized_sentences[0])
-
-        self.rnn_theano(vocabulary_size, word_to_index, tokenized_sentences)
-
+        self.rnn_theano(word_to_index, index_to_word, tokenized_sentences)
         return ok
 
-    def rnn_theano(self, vocabulary_size, word_to_index, tokenized_sentences):
-        import numpy
-
-        numpy.random.seed(10)
-
+    def rnn_theano(self, word_to_index, index_to_word, tokenized_sentences):
         # Create the training data
         x_train = numpy.asarray([[word_to_index[w] for w in sent[:-1]] for sent in tokenized_sentences])
         y_train = numpy.asarray([[word_to_index[w] for w in sent[1:]] for sent in tokenized_sentences])
 
-        model = self.get_theano_model(vocabulary_size, x_train, y_train)
+        model = self.get_theano_model(x_train, y_train)
         model.sgd_step(x_train[10], y_train[10], 0.005)
 
-    def get_theano_model(self, vocabulary_size, x_train, y_train):
-        from neuralnet.nn.utils import Utils
+        num_sentences = 10
+        sentence_min_length = 7
+
+        for i in range(num_sentences):
+            sent = []
+            # We want long sentences, not sentences with one or two words
+            while len(sent) < sentence_min_length:
+                sent = self.generate_sentence(model, word_to_index, index_to_word)
+            print(" ".join(sent))
+
+    def generate_sentence(self, model, word_to_index, index_to_word):
+        # We start the sentence with the start token
+        new_sentence = [word_to_index[self.sentence_start_token]]
+        # Repeat until we get an end token
+        while not new_sentence[-1] == word_to_index[self.sentence_end_token]:
+            #next_word_probs = model.forward_propagation(new_sentence)
+            next_word_probs = model.predict(new_sentence)
+            sampled_word = word_to_index[self.unknown_token]
+
+            # We don't want to sample unknown words
+            while sampled_word == word_to_index[self.unknown_token]:
+                samples = numpy.random.multinomial(1, next_word_probs[-1])
+                sampled_word = numpy.argmax(samples)
+                #logger.debug("sampled_word: %s" % index_to_word[sampled_word])
+
+            new_sentence.append(sampled_word)
+
+        sentence_str = [index_to_word[x] for x in new_sentence[1:-1]]
+        return sentence_str
+
+    def get_theano_model(self, x_train, y_train):
         from neuralnet.nn.rnn_theano import RNNTheano
-        import os.path
+
         trained_model_data = './data/trained-model-theano.npz'
 
-        model = RNNTheano(vocabulary_size, hidden_dim=50)
+        model = RNNTheano(self.vocabulary_size, hidden_dim=50)
         if os.path.isfile(trained_model_data):
             logger.info("found trained model, loading instead of training new: %s" % trained_model_data)
             Utils.load_model_parameters_theano(trained_model_data, model)
         else:
             logger.info("training new model...")
+            numpy.random.seed(10)
+
             self.train_with_sgd(model, x_train, y_train, nepoch=5)
+
             logger.info("saving trained model to: %s" % trained_model_data)
             Utils.save_model_parameters_theano(trained_model_data, model)
 
         return model
 
+    # Outer SGD Loop
+    # - model: The RNN model instance
+    # - X_train: The training data set
+    # - y_train: The training data labels
+    # - learning_rate: Initial learning rate for SGD
+    # - nepoch: Number of times to iterate through the complete dataset
+    # - evaluate_loss_after: Evaluate the loss after this many epochs
+    def train_with_sgd(self, model, x_train, y_train, learning_rate=0.005, nepoch=100, evaluate_loss_after=5):
+        from datetime import datetime
+        from time import time
+
+        # We keep track of the losses so we can plot them later
+        losses = []
+        num_examples_seen = 0
+        total_steps_to_train = len(y_train)*nepoch
+        total_time = 0
+
+        for epoch in range(nepoch):
+            # Optionally evaluate the loss
+            if epoch > 0 and epoch % evaluate_loss_after == 0:
+                loss = model.calculate_loss(x_train, y_train)
+                losses.append((num_examples_seen, loss))
+                time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+                logger.info("%s: Loss after num_examples_seen=%d epoch=%d: %f" % (time, num_examples_seen, epoch, loss))
+                # Adjust the learning rate if loss increases
+                if len(losses) > 1 and losses[-1][1] > losses[-2][1]:
+                    learning_rate *= 0.5
+                    logger.info("Setting learning rate to %f" % learning_rate)
+
+            # For each training example...
+            for i in range(len(y_train)):
+                # One SGD step
+                before = time()
+                model.sgd_step(x_train[i], y_train[i], learning_rate)
+                after = time()
+
+                num_examples_seen += 1
+                total_time += (after - before)*1000
+                average_step_time = total_time/num_examples_seen
+                self.log_time(i, epoch, nepoch, average_step_time, total_steps_to_train, num_examples_seen)
+
+    def log_time(self, i, epoch, nepoch, average_step_time, total_steps_to_train, num_examples_seen):
+        if i % 100 != 0:
+            return
+
+        from math import floor
+        avg_step_time = round(average_step_time, 2)
+        est_total_time = average_step_time*total_steps_to_train
+        est_elapsed_time = average_step_time*num_examples_seen
+        time_left = floor((est_total_time - est_elapsed_time) / 1000)
+
+        formatted_time = "%ss" % time_left
+        if time_left > 3600:
+            seconds = time_left % 60
+            minutes = ((time_left - seconds)/60) % 60
+            hours = (time_left - minutes*60 - seconds) / 3600
+            formatted_time = "%s hours, %s minutes, %s seconds" % (int(hours), int(minutes), int(seconds))
+        elif time_left > 60:
+            seconds = time_left % 60
+            minutes = (time_left - seconds)/60
+            formatted_time = "%s minutes, %s seconds" % (int(minutes), int(seconds))
+
+        logger.debug("epoch %s/%s, examples %s/%s, avg step time %sms, time left %s" %
+                     (epoch, nepoch, num_examples_seen, total_steps_to_train,
+                     avg_step_time, formatted_time))
+
     def rnn_numpy(self, vocabulary_size, word_to_index, tokenized_sentences):
-        import numpy
         from neuralnet.nn.rnn_numpy import RNNNumpy
 
         numpy.random.seed(10)
@@ -136,36 +196,3 @@ class TextCommand:
         # Train on a small subset of the data to see what happens
         model = RNNNumpy(vocabulary_size)
         self.train_with_sgd(model, X_train[:100], y_train[:100], nepoch=10, evaluate_loss_after=1)
-
-    # Outer SGD Loop
-    # - model: The RNN model instance
-    # - X_train: The training data set
-    # - y_train: The training data labels
-    # - learning_rate: Initial learning rate for SGD
-    # - nepoch: Number of times to iterate through the complete dataset
-    # - evaluate_loss_after: Evaluate the loss after this many epochs
-    def train_with_sgd(self, model, x_train, y_train, learning_rate=0.005, nepoch=100, evaluate_loss_after=5):
-        from datetime import datetime
-
-        # We keep track of the losses so we can plot them later
-        losses = []
-        num_examples_seen = 0
-
-        for epoch in range(nepoch):
-            # Optionally evaluate the loss
-            if epoch % evaluate_loss_after == 0:
-                loss = model.calculate_loss(x_train, y_train)
-                losses.append((num_examples_seen, loss))
-                time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-                logger.info("%s: Loss after num_examples_seen=%d epoch=%d: %f" % (time, num_examples_seen, epoch, loss))
-                # Adjust the learning rate if loss increases
-                if len(losses) > 1 and losses[-1][1] > losses[-2][1]:
-                    learning_rate = learning_rate * 0.5
-                    logger.info("Setting learning rate to %f" % learning_rate)
-                #sys.stdout.flush()
-
-            # For each training example...
-            for i in range(len(y_train)):
-                # One SGD step
-                model.sgd_step(x_train[i], y_train[i], learning_rate)
-                num_examples_seen += 1
--- neuralnet/nn/rnn_theano.py
+++ neuralnet/nn/rnn_theano.py
@@ -1,69 +1,134 @@
 __author__ = 'Oscar Eriksson <oscar.eriks@gmail.com>'
 
-import numpy as np
 import theano as theano
 import theano.tensor as T
 from .utils import *
 import operator
-import logging
 
 
 class RNNTheano:
-    logger = logging.getLogger(__name__)
-
     def __init__(self, word_dim, hidden_dim=100, bptt_truncate=4):
         # Assign instance variables
         self.word_dim = word_dim
         self.hidden_dim = hidden_dim
         self.bptt_truncate = bptt_truncate
-        # Randomly initialize the network parameters
-        U = np.random.uniform(-np.sqrt(1./word_dim), np.sqrt(1./word_dim), (hidden_dim, word_dim))
+        # Initialize the network parameters
+        E = np.random.uniform(-np.sqrt(1./word_dim), np.sqrt(1./word_dim), (hidden_dim, word_dim))
+        U = np.random.uniform(-np.sqrt(1./hidden_dim), np.sqrt(1./hidden_dim), (6, hidden_dim, hidden_dim))
+        W = np.random.uniform(-np.sqrt(1./hidden_dim), np.sqrt(1./hidden_dim), (6, hidden_dim, hidden_dim))
         V = np.random.uniform(-np.sqrt(1./hidden_dim), np.sqrt(1./hidden_dim), (word_dim, hidden_dim))
-        W = np.random.uniform(-np.sqrt(1./hidden_dim), np.sqrt(1./hidden_dim), (hidden_dim, hidden_dim))
+        b = np.zeros((6, hidden_dim))
+        c = np.zeros(word_dim)
         # Theano: Created shared variables
+        self.E = theano.shared(name='E', value=E.astype(theano.config.floatX))
         self.U = theano.shared(name='U', value=U.astype(theano.config.floatX))
-        self.V = theano.shared(name='V', value=V.astype(theano.config.floatX))
         self.W = theano.shared(name='W', value=W.astype(theano.config.floatX))
+        self.V = theano.shared(name='V', value=V.astype(theano.config.floatX))
+        self.b = theano.shared(name='b', value=b.astype(theano.config.floatX))
+        self.c = theano.shared(name='c', value=c.astype(theano.config.floatX))
+        # SGD / rmsprop: Initialize parameters
+        self.mE = theano.shared(name='mE', value=np.zeros(E.shape).astype(theano.config.floatX))
+        self.mU = theano.shared(name='mU', value=np.zeros(U.shape).astype(theano.config.floatX))
+        self.mV = theano.shared(name='mV', value=np.zeros(V.shape).astype(theano.config.floatX))
+        self.mW = theano.shared(name='mW', value=np.zeros(W.shape).astype(theano.config.floatX))
+        self.mb = theano.shared(name='mb', value=np.zeros(b.shape).astype(theano.config.floatX))
+        self.mc = theano.shared(name='mc', value=np.zeros(c.shape).astype(theano.config.floatX))
         # We store the Theano graph here
         self.theano = {}
         self.__theano_build__()
 
     def __theano_build__(self):
-        U, V, W = self.U, self.V, self.W
+        E, V, U, W, b, c = self.E, self.V, self.U, self.W, self.b, self.c
+
         x = T.ivector('x')
         y = T.ivector('y')
-        def forward_prop_step(x_t, s_t_prev, U, V, W):
-            s_t = T.tanh(U[:,x_t] + W.dot(s_t_prev))
-            o_t = T.nnet.softmax(V.dot(s_t))
-            return [o_t[0], s_t]
-        [o,s], updates = theano.scan(
+
+        def forward_prop_step(x_t, s_t1_prev, s_t2_prev):
+            # This is how we calculated the hidden state in a simple RNN. No longer!
+            # s_t = T.tanh(U[:,x_t] + W.dot(s_t1_prev))
+
+            # Word embedding layer
+            x_e = E[:,x_t]
+
+            # GRU Layer 1
+            z_t1 = T.nnet.hard_sigmoid(U[0].dot(x_e) + W[0].dot(s_t1_prev) + b[0])
+            r_t1 = T.nnet.hard_sigmoid(U[1].dot(x_e) + W[1].dot(s_t1_prev) + b[1])
+            c_t1 = T.tanh(U[2].dot(x_e) + W[2].dot(s_t1_prev * r_t1) + b[2])
+            s_t1 = (T.ones_like(z_t1) - z_t1) * c_t1 + z_t1 * s_t1_prev
+
+            # GRU Layer 2
+            z_t2 = T.nnet.hard_sigmoid(U[3].dot(s_t1) + W[3].dot(s_t2_prev) + b[3])
+            r_t2 = T.nnet.hard_sigmoid(U[4].dot(s_t1) + W[4].dot(s_t2_prev) + b[4])
+            c_t2 = T.tanh(U[5].dot(s_t1) + W[5].dot(s_t2_prev * r_t2) + b[5])
+            s_t2 = (T.ones_like(z_t2) - z_t2) * c_t2 + z_t2 * s_t2_prev
+
+            # Final output calculation
+            # Theano's softmax returns a matrix with one row, we only need the row
+            o_t = T.nnet.softmax(V.dot(s_t2) + c)[0]
+
+            return [o_t, s_t1, s_t2]
+
+        [o, s, s2], updates = theano.scan(
             forward_prop_step,
             sequences=x,
-            outputs_info=[None, dict(initial=T.zeros(self.hidden_dim))],
-            non_sequences=[U, V, W],
             truncate_gradient=self.bptt_truncate,
-            strict=True)
+            outputs_info=[None,
+                          dict(initial=T.zeros(self.hidden_dim)),
+                          dict(initial=T.zeros(self.hidden_dim))])
 
         prediction = T.argmax(o, axis=1)
         o_error = T.sum(T.nnet.categorical_crossentropy(o, y))
 
+        # Total cost (could add regularization here)
+        cost = o_error
+
         # Gradients
-        dU = T.grad(o_error, U)
-        dV = T.grad(o_error, V)
-        dW = T.grad(o_error, W)
+        dE = T.grad(cost, E)
+        dU = T.grad(cost, U)
+        dW = T.grad(cost, W)
+        db = T.grad(cost, b)
+        dV = T.grad(cost, V)
+        dc = T.grad(cost, c)
 
         # Assign functions
-        self.forward_propagation = theano.function([x], o)
-        self.predict = theano.function([x], prediction)
-        self.ce_error = theano.function([x, y], o_error)
-        self.bptt = theano.function([x, y], [dU, dV, dW])
+        self.predict = theano.function([x], o)
+        self.predict_class = theano.function([x], prediction)
+        self.ce_error = theano.function([x, y], cost)
+        self.bptt = theano.function([x, y], [dE, dU, dW, db, dV, dc])
 
-        # SGD
+        # SGD parameters
         learning_rate = T.scalar('learning_rate')
-        self.sgd_step = theano.function([x,y,learning_rate], [],
-                      updates=[(self.U, self.U - learning_rate * dU),
-                              (self.V, self.V - learning_rate * dV),
-                              (self.W, self.W - learning_rate * dW)])
+        decay = T.scalar('decay')
+
+        # rmsprop cache updates
+        mE = decay * self.mE + (1 - decay) * dE ** 2
+        mU = decay * self.mU + (1 - decay) * dU ** 2
+        mW = decay * self.mW + (1 - decay) * dW ** 2
+        mV = decay * self.mV + (1 - decay) * dV ** 2
+        mb = decay * self.mb + (1 - decay) * db ** 2
+        mc = decay * self.mc + (1 - decay) * dc ** 2
+
+        self.sgd_step = theano.function(
+            [x, y, learning_rate, theano.Param(decay, default=0.9)],
+            [],
+            updates=[(E, E - learning_rate * dE / T.sqrt(mE + 1e-6)),
+                     (U, U - learning_rate * dU / T.sqrt(mU + 1e-6)),
+                     (W, W - learning_rate * dW / T.sqrt(mW + 1e-6)),
+                     (V, V - learning_rate * dV / T.sqrt(mV + 1e-6)),
+                     (b, b - learning_rate * db / T.sqrt(mb + 1e-6)),
+                     (c, c - learning_rate * dc / T.sqrt(mc + 1e-6)),
+                     (self.mE, mE),
+                     (self.mU, mU),
+                     (self.mW, mW),
+                     (self.mV, mV),
+                     (self.mb, mb),
+                     (self.mc, mc)
+                     ])
+
+    def forward_prop_step_vanilla(x_t, s_t_prev, U, V, W):
+        s_t = T.tanh(U[:, x_t] + W.dot(s_t_prev))
+        o_t = T.nnet.softmax(V.dot(s_t))
+        return [o_t[0], s_t]
 
     def calculate_total_loss(self, X, Y):
         return np.sum([self.ce_error(x,y) for x,y in zip(X,Y)])
@@ -73,20 +138,20 @@ class RNNTheano:
         num_words = np.sum([len(y) for y in Y])
         return self.calculate_total_loss(X, Y)/float(num_words)
 
-    def gradient_check_theano(model, x, y, h=0.001, error_threshold=0.01):
+    def gradient_check_theano(self, x, y, h=0.001, error_threshold=0.01):
         # Overwrite the bptt attribute. We need to backpropagate all the way to get the correct gradient
-        model.bptt_truncate = 1000
+        self.bptt_truncate = 1000
         # Calculate the gradients using backprop
-        bptt_gradients = model.bptt(x, y)
+        bptt_gradients = self.bptt(x, y)
         # List of all parameters we want to chec.
         model_parameters = ['U', 'V', 'W']
 
         # Gradient check for each parameter
         for pidx, pname in enumerate(model_parameters):
             # Get the actual parameter value from the mode, e.g. model.W
-            parameter_T = operator.attrgetter(pname)(model)
+            parameter_T = operator.attrgetter(pname)(self)
             parameter = parameter_T.get_value()
-            logger.info("Performing gradient check for parameter %s with size %d." % (pname, np.prod(parameter.shape)))
+            self.logger.info("Performing gradient check for parameter %s with size %d." % (pname, np.prod(parameter.shape)))
 
             # Iterate over each element of the parameter matrix, e.g. (0,0), (0,1), ...
             it = np.nditer(parameter, flags=['multi_index'], op_flags=['readwrite'])
@@ -98,10 +163,10 @@ class RNNTheano:
                 # Estimate the gradient using (f(x+h) - f(x-h))/(2*h)
                 parameter[ix] = original_value + h
                 parameter_T.set_value(parameter)
-                gradplus = model.calculate_total_loss([x],[y])
+                gradplus = self.calculate_total_loss([x],[y])
                 parameter[ix] = original_value - h
                 parameter_T.set_value(parameter)
-                gradminus = model.calculate_total_loss([x],[y])
+                gradminus = self.calculate_total_loss([x],[y])
                 estimated_gradient = (gradplus - gradminus)/(2*h)
                 parameter[ix] = original_value
                 parameter_T.set_value(parameter)
